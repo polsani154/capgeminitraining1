d9c630d62ab216a4c36bdec7cb90092eee5432fa
Polsani sandeep <polsani.sandeep@gmail.com>
2020-02-14 12:17:03 +0530

    sprint2

---
+++ Sprint-1/src/Repository/Ticket.java
@@ -0,0 +1,64 @@
+package Repository;
+
+public class Ticket {
+
+	private Integer ticketId;
+	private Integer noOfSeats;
+	private String[] seatName;
+	private Booking bookingRef;
+	private Boolean ticketStatus;
+	private String screenName;
+	
+	public Ticket() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getTicketId() {
+		return ticketId;
+	}
+
+	public void setTicketId(Integer ticketId) {
+		this.ticketId = ticketId;
+	}
+
+	public Integer getNoOfSeats() {
+		return noOfSeats;
+	}
+
+	public void setNoOfSeats(Integer noOfSeats) {
+		this.noOfSeats = noOfSeats;
+	}
+
+	public String[] getSeatName() {
+		return seatName;
+	}
+
+	public void setSeatName(String[] seatName) {
+		this.seatName = seatName;
+	}
+
+	public Booking getBookingRef() {
+		return bookingRef;
+	}
+
+	public void setBookingRef(Booking bookingRef) {
+		this.bookingRef = bookingRef;
+	}
+
+	public Boolean getTicketStatus() {
+		return ticketStatus;
+	}
+
+	public void setTicketStatus(Boolean ticketStatus) {
+		this.ticketStatus = ticketStatus;
+	}
+
+	public String getScreenName() {
+		return screenName;
+	}
+
+	public void setScreenName(String screenName) {
+		this.screenName = screenName;
+	}
+
+}
